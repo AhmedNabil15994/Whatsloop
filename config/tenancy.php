e8c7124dbcb40811a75b833acfc9f20f52f9b966
Ahmed Nabil <mody998866@gmail.com>
2021-03-02 04:29:04 +0200

    Fix

--- config/tenancy.php
+++ config/tenancy.php
@@ -18,7 +18,7 @@ return [
      * Only relevant if you're using the domain or subdomain identification middleware.
      */
     'central_domains' => [
-        'newsystem.localhost',
+        'whatsloop.localhost',
         //'127.0.0.1',
         //'localhost',
     ],
